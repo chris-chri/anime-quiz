9bd06c0436510d377960a6ea120be2aef96da4ff
chris-chri <168101977+chris-chri@users.noreply.github.com>
2025-06-23 19:28:27 +0100

    update 1.0 is going to be the first recoreded update for this program. I have finshed the attack on titan quiz section and for the bleach quiz it is still unfinshed and needs a bit of work. Both quizes have the victory and lossing screens, the gifs seem like they are working perfectly.

---
+++ attack-victory.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("dramaticText").classList.add("")
+    })
+})
+
+function restartQuiz(){
+    window.location.href = "index.html";
+}
---
+++ bleach.js
@@ -0,0 +1,102 @@
+const quizData = [
+    
+    {
+    question: "Who is the main antagonist in Bleach: Thousand-Year Bldood War?",
+    answers: ["Aizen","Ichigo Kurosaki","Yhwach","Uryu Ishida","Byakuya Kuchiki"],
+    correct: 2,
+    },
+
+{ 
+    question:"What is the name of the group led by Yhwach ?",
+    answers: ["The Sternritter","The Espada","The Gotei 13","The Visored","The Arrancar"],
+    correct: 0,
+
+},
+
+{
+    question: "What is the special ability of Yhwach`s 'The Almighty' power?",
+    answers: ["Time Manipulation","Reality alteration","Immense strength","Future sight and alteration","Healing regeneration"],
+    correct: 3,
+},
+
+{
+    question : "What is the name of Ichigo's Zanpakuto, revealed in the newest season?",
+    answers:["Zangetsu","Mugestu","Getsuga Tensho","Tensa Zangestu","True Zangetsu"],
+    correct: 4,
+},
+
+{
+    question:" What does The 'Auswählen' ability used by Yhwatch?",
+    answers: ["Steal Bankai powers","Steal Shinigami powers","Steal Quincy powers","Steal Hollow powers","Steal Arrancar powers"],
+    correct: 2,
+}
+
+]
+
+let currentQuestion = 0
+let selectedAnswer = null
+let score = 0
+
+const questionElement =document.getElementById("question1")
+const answersElement = document.getElementById("answers1")
+const nextBtn = document.getElementById("nextBtn1")
+const questionCounter = document.getElementById("questionCounter1")
+const progressFill = document.getElementById("progressFill1")
+
+function loadQuestion(){
+    const current = quizData[currentQuestion]
+    questionElement.textContent = current.question
+    answersElement.innerHTML = ""
+    current.answers.forEach((answer,index) =>{
+        const button = document.createElement("button")
+        button.className = "answer-btn"
+        button.textContent = answer
+        button.onclick = () => selectAnswer(button, index)
+        answersElement.appendChild(button)
+    })
+
+    questionCounter.textContent = `Question ${currentQuestion + 1} of ${quizData.length}`
+    const progress = ((currentQuestion + 1) / quizData.length) * 100
+    progressFill.style.width = progress + "%"
+
+
+    selectedAnswer = null
+    nextBtn.classList.remove("active")
+}
+
+function selectAnswer(button,answerIndex){
+    document.querySelectorAll(".answer-btn").forEach((btn) =>{
+        btn.classList.remove("selected")
+
+    })
+
+    button.classList.add("selected")
+    selectedAnswer = answerIndex
+    nextBtn.classList.add("active")
+}
+
+function nextQuestion(){
+    if (selectedAnswer === null) return
+
+    if (selectedAnswer === quizData[currentQuestion].correct){
+        score++
+    }
+
+    currentQuestion++
+
+    if (currentQuestion < quizData.length) {
+        loadQuestion()
+    }  else {
+        if  (score >= 4) {
+            window.location.href = "bleach-win.html"
+        } else {
+
+            window.location.href = "bleach-loss.html"
+
+        }
+    }
+}
+
+nextBtn.addEventListener("click", nextQuestion)
+
+loadQuestion()
